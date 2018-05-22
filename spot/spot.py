5f2a687418a2320143536c1358a6fab2a35c7834
sohumsontakke <sohumsontakke@gmail.com>
2018-05-22 01:12:19 -0400

    Added spot file for sending album art url in email

--- spot/spot.py
+++ spot/spot.py
@@ -18,23 +18,29 @@ if internet_available():
 	# checks if the program has run today
 	if not run_today():
 
-		# gets list of all artists from users public playlists
-		all_artists = parse_playlists_for_artists(spot, username)
+		try:
+			# gets list of all artists from users public playlists
+			all_artists = parse_playlists_for_artists(spot, username)
 
-		# gets list with all artist information for today
-		all_artist_info = get_all_artists_info(spot, all_artists)
+			# gets list with all artist information for today
+			all_artist_info = get_all_artists_info(spot, all_artists)
 
-		# writes todays data to csv file
-		write_to_CSV(all_artist_info)
+			# writes todays data to csv file
+			write_to_CSV(all_artist_info)
+			
+			# deletes extra .csv files if there are more than three
+			del_prev_files()
+
+			# gets the list of artists with new albums
+			artists_with_new_albums = get_artists_with_new_albums(spot, prev_artist_info, all_artist_info)
+
+			# sends email if there is an artist with a new album
+			notify_new_album(spot, artists_with_new_albums)
 		
-		# deletes extra .csv files if there are more than three
-		del_prev_files()
+		except ConnectionResetError as err:
 
-		# gets the list of artists with new albums
-		artists_with_new_albums = get_artists_with_new_albums(prev_artist_info, all_artist_info)
-
-		# sends email if there is an artist with a new album
-		notify_new_album(spot, artists_with_new_albums)
+			print("Error establishing connection")
 
 else:
+	
 	print("No internet connection at this time")
