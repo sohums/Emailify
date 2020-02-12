b4c6ae34e50d62b71279a91d17a61346cf44a056
sohums <sohumsontakke@gmail.com>
2020-02-12 02:50:06 +0000

    Changed quotes to apostrophes to match styling

--- spot/csvHandler.py
+++ spot/csvHandler.py
@@ -22,50 +22,50 @@ def write_to_CSV(all_info):
 def read_from_CSV():
     today = date.today()
     today_str = today.strftime('%y%m%d')
-    today_filename = today_str + ".csv"
+    today_filename = today_str + '.csv'
 
-    path, dirs, files = next(os.walk("../data/"))
+    path, dirs, files = next(os.walk('../data/'))
     prev_artist_info = []
     all_csv_files = []
     valid_prev_data = False
 
     # check for valid .csv file
     for file in files:
-        if ".csv" in file:
+        if '.csv' in file:
             valid_prev_data = True
-            all_csv_files.append(file.split(".")[0])
+            all_csv_files.append(file.split('.')[0])
 
     all_csv_files = sorted(all_csv_files, reverse=True)
 
     # check previous file closest to today
     if valid_prev_data:
-        with open("../data/" + all_csv_files[0] + ".csv") as csv_file:
+        with open('../data/' + all_csv_files[0] + '.csv') as csv_file:
             reader = csv.reader(csv_file)
             for row in reader:
                 # ast converts string list to type list
                 artist = Artist(row[0], row[1], ast.literal_eval(row[2]))
                 prev_artist_info.append(artist)
     else:
-        print("No data to read from")
+        print('No data to read from')
 
     return prev_artist_info
 
 # deletes extra .csv files if there are more than three
 def del_extra_files():
-    path, dirs, files = next(os.walk("../data/"))
+    path, dirs, files = next(os.walk('../data/'))
     all_csv_files = []
 
     for file in files:
-        if ".csv" in file:
+        if '.csv' in file:
             valid_prev_data = True
-            all_csv_files.append(file.split(".")[0])
+            all_csv_files.append(file.split('.')[0])
 
     all_csv_files = sorted(all_csv_files, reverse=True)
     file_count = len(all_csv_files)
 
     # while more than three files delete last file
     while file_count > 3:
-        os.remove("../data/" + all_csv_files[-1] + ".csv")
+        os.remove('../data/' + all_csv_files[-1] + '.csv')
         del all_csv_files[-1]
         file_count -= 1
 
@@ -73,21 +73,21 @@ def del_extra_files():
 def run_today():
     today = date.today()
     today_str = today.strftime('%y%m%d')
-    today_filename = today_str + ".csv"
+    today_filename = today_str + '.csv'
 
-    path, dirs, files = next(os.walk("../data/"))
+    path, dirs, files = next(os.walk('../data/'))
 
     if today_filename in files:
-        print("The program has already run today")
+        print('The program has already run today')
         return True
     else:
         return False
 
 def data_not_present():
-    path, dirs, files = next(os.walk("../data/"))
+    path, dirs, files = next(os.walk('../data/'))
     csv_file_count = 0
 
     for file in files:
-        if ".csv" in file:
+        if '.csv' in file:
             csv_file_count += 1
     return True if csv_file_count < 1 else False
--- spot/emailer.py
+++ spot/emailer.py
@@ -8,32 +8,32 @@ def send_email(list_of_artists):
 
     if len(list_of_artists) == 1:
         newMusicArtist = list_of_artists[0]
-        SUBJECT = newMusicArtist.name + " has released a new album!"
-        TEXT = str("A new album from " + newMusicArtist.name + " just released! The album name is "
-                + newMusicArtist.newAlbum + ". \nCheck it out at: " + newMusicArtist.artistId
-                + ". \nAlbum artwork: " + newMusicArtist.newAlbumArt)
+        SUBJECT = newMusicArtist.name + ' has released a new album!'
+        TEXT = str('A new album from ' + newMusicArtist.name + ' just released! The album name is '
+                + newMusicArtist.newAlbum + '. \nCheck it out at: ' + newMusicArtist.artistId
+                + '. \nAlbum artwork: ' + newMusicArtist.newAlbumArt)
     else:
         SUBJECT = 'New music from your artists on Spotify!'
         TEXT = ''
         for newMusicArtist in list_of_artists:
-            TEXT += str("A new album from " + newMusicArtist.name + " just released! The album name is "
-                    + newMusicArtist.newAlbum + ". \nCheck it out at: " + newMusicArtist.artistId
-                    + ". \nAlbum artwork: " + newMusicArtist.newAlbumArt + "\n\n")
+            TEXT += str('A new album from ' + newMusicArtist.name + ' just released! The album name is '
+                    + newMusicArtist.newAlbum + '. \nCheck it out at: ' + newMusicArtist.artistId
+                    + '. \nAlbum artwork: ' + newMusicArtist.newAlbumArt + '\n\n')
 
     # Prepare actual message
     message = """From: %s\nTo: %s\nSubject: %s\n\n%s
 	""" % (FROM, ", ".join(TO), SUBJECT, TEXT)
     try:
-        server = smtplib.SMTP("smtp.gmail.com", 587)
+        server = smtplib.SMTP('smtp.gmail.com', 587)
         server.ehlo()
         server.starttls()
         server.login(email_from, email_from_pwd)
         server.sendmail(FROM, TO, message.encode('utf8'))
         server.close()
-        print("Successfully sent email")
+        print('Successfully sent email')
         return True
     except Exception as e:
         print(str(e))
-        print("Failed to send email")
+        print('Failed to send email')
         print(TEXT)
         return False
